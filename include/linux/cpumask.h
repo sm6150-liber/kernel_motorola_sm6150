9240b827f0ca937bcb03f96373a6267ac59215b2
Panchajanya1999 <panchajanya@azure-dev.live>
2021-04-12 20:39:24 +0530

    cpumask: Add cpumasks for first and second Big CPUs on Big cluster
    Required for placing tasks on a dedicated Big CPU by affining IRQ or
    kthreads to a certain CPU in a heterogeneous system.
    
    Change-Id: I1debcbacbbb790b1fcc71221f7b9e8b5f43fd57e
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -57,6 +57,8 @@ extern unsigned int nr_cpu_ids;
  *     cpu_isolated_mask- has bit 'cpu' set iff cpu isolated
  *     cpu_lp_mask      - has bit 'cpu' set iff cpu is part of little cluster
  *     cpu_perf_mask    - has bit 'cpu' set iff cpu is part of big cluster
+ *     cpu_perf_first_mask- has bit 'cpu' set iff cpu is a part of big cluster and affined for tasks on cpu6
+ *     cpu_perf_second_mask- has bit 'cpu' set iff cpu is a part of big cluster and affined for tasks on cpu7
  *
  *  If !CONFIG_HOTPLUG_CPU, present == possible, and active == online.
  *
@@ -101,6 +103,8 @@ extern struct cpumask __cpu_isolated_mask;
 #define cpu_isolated_mask ((const struct cpumask *)&__cpu_isolated_mask)
 extern const struct cpumask *const cpu_lp_mask;
 extern const struct cpumask *const cpu_perf_mask;
+extern const struct cpumask *const cpu_perf_first_mask;
+extern const struct cpumask *const cpu_perf_second_mask;
 
 #if NR_CPUS > 1
 #define num_online_cpus()	cpumask_weight(cpu_online_mask)
