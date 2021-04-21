f1339cb5e42c434e6fab85f8423d1b72b6e5db51
KenHV <yo@kenharris.xyz>
2021-04-21 19:27:09 +0530

    input: focaltech: Silence verbose logging
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- drivers/input/touchscreen/focaltech_0flash_mmi/focaltech_common.h
+++ drivers/input/touchscreen/focaltech_0flash_mmi/focaltech_common.h
@@ -159,22 +159,14 @@ struct ts_ic_info {
     printk("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__); \
 } while (0)
 #else /* #if FTS_DEBUG_EN*/
-#define FTS_DEBUG(fmt, args...) do { \
-    pr_debug("[FTS_TS]%s:"fmt"\n", __func__, ##args); \
-} while (0)
+#define FTS_DEBUG(fmt, args...) do { } while (0)
 
-#define FTS_FUNC_ENTER() do { \
-    pr_debug("[FTS_TS]%s: Enter\n", __func__); \
-} while (0)
+#define FTS_FUNC_ENTER() do { } while (0)
 
-#define FTS_FUNC_EXIT() do { \
-    pr_debug("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__); \
-} while (0)
+#define FTS_FUNC_EXIT() do { } while (0)
 #endif
 
-#define FTS_INFO(fmt, args...) do { \
-    printk(KERN_INFO "[FTS_TS/I]%s:"fmt"\n", __func__, ##args); \
-} while (0)
+#define FTS_INFO(fmt, args...) do { } while (0)
 
 #define FTS_ERROR(fmt, args...) do { \
     printk(KERN_ERR "[FTS_TS/E]%s:"fmt"\n", __func__, ##args); \
