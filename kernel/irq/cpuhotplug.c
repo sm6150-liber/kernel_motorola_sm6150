97374f41dca52ee1805014520447c0a0cf4f9a1f
KenHV <yo@kenharris.xyz>
2021-04-22 08:09:06 +0530

    irq: Silence logspam when IRQs are migrated away from offline CPU
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- kernel/irq/cpuhotplug.c
+++ kernel/irq/cpuhotplug.c
@@ -191,20 +191,13 @@ void irq_migrate_all_off_this_cpu(void)
 	unsigned int irq;
 
 	for_each_active_irq(irq) {
-		bool affinity_broken;
-
 		desc = irq_to_desc(irq);
 		if (!desc)
 			continue;
 
 		raw_spin_lock(&desc->lock);
-		affinity_broken = migrate_one_irq(desc);
+		migrate_one_irq(desc);
 		raw_spin_unlock(&desc->lock);
-
-		if (affinity_broken) {
-			pr_info_ratelimited("IRQ %u: no longer affine to CPU%u\n",
-					    irq, smp_processor_id());
-		}
 	}
 }
 
